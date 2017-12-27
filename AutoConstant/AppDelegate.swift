9261e31ecf6649cd939da8cdac3e17b868c8fefc
Jack Zhao <jzhao@connectedlab.com>
2017-12-27 16:03:24 -0500

    Initial Commit

---
+++ AutoConstant/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  AutoConstant
+//
+//  Created by Xiongbin Zhao on 2017-12-27.
+//  Copyright © 2017 Xiongbin Zhao. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
